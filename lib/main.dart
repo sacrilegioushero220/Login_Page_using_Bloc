71c20b9ff33889e78e03c39badf84c59ee61e489
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-12-07 19:44:13 +0530

    Login Page using Bloc

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/login_screen.dart';
+import 'package:login_page/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
